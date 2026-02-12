9e3d31384cb3d19c3bf7c6b71c33df28a8403fb3
Jono Sommers <jonosommers@Jonos-MacBook-Pro.local>
2026-02-12 16:50:27 -0600

    added header and players

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,19 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li><span class="player-name">Player1</span></li>
+          <span className="player-symbol">X</span>
+          <li><span class="player-name">Player2</span></li>
+          <span className="player-symbol">O</span>
+        </ol>
+
+        GAMEBOARD
+      </div>
+
+      LOG
+    </main>
   )
 }
 
